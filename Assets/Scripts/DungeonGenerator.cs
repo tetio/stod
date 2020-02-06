2b966e1b2449271886d818400abd930e4d4dd450
Tetio <tetio@live.com>
2020-02-06 18:15:01 +0100

    introducing wall generator

---
+++ Assets/Scripts/DungeonGenerator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DungeonGenerator : MonoBehaviour {
+    List<Vector3> wall = new List<Vector3> ();
+    [SerializeField]
+    private GameObject brick;
+    void Awake () {
+        wall.Add (new Vector3 (9.5f, 0.5f, 5.5f));
+        wall.Add (new Vector3 (11.5f, 0.5f, 5.5f));
+        wall.Add (new Vector3 (9.5f, 0.5f, 6.5f));
+        wall.Add (new Vector3 (11.5f, 0.5f, 6.5f));
+
+        foreach (Vector3 position in wall) {
+            Instantiate(brick, position, Quaternion.identity);
+        }
+    }
+
+}
